62d75a9776a4fb7af8d94a4f2c9e8e65c25129b5
simonl <simon.lesly@yahoo.com>
2024-03-26 11:32:13 +0100

    sauvegarde du 26 mars 2024(11h32)

--- src/app/service/manage-paniers.service.ts
+++ src/app/service/manage-paniers.service.ts
@@ -12,8 +12,8 @@ import { LigneBoxes } from '../models/LigneBoxes';
 export class ManagePaniersService {
 panier:Panier
   constructor() { 
-    let lignes:Array<LigneBoxes> = JSON.parse(localStorage.getItem("panier") ?? "[]")
-    this.panier=new Panier(1,lignes,false)
+    this. panier =JSON.parse(localStorage.getItem("panier") ?? "{'id':1,'listeDeBox':[],'statut':false}")
+   
   }
   //méthode pour consulter le contenue du panier
   public getPanier(){
@@ -24,6 +24,7 @@ panier:Panier
   public addBoxes(uneBox:Boxes,qte:number){
     //TODO
     let estPresente=false
+    console.log(this.panier)
     //ETAPE 1 : Parcourir le panier pour savoir si la boxe il y est deja 
     for (let uneLigne of this.panier.listeDeBoxe) {
       if(uneLigne.boxe.id == uneBox.id){// Si oui alors tu modifie la quantite  et estPresnte devient true
