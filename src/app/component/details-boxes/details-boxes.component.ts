7c49814319518ba64b71be5ed5e3d6509713b0db
sio <lufunduoceane@gmail.com>
2024-03-26 09:42:55 +0100

    Mise à jour avec test de production

--- src/app/component/details-boxes/details-boxes.component.ts
+++ src/app/component/details-boxes/details-boxes.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
 import { ManageBoxesService } from '../../service/manage-boxes.service';
 import { Boxes } from '../../modules/Boxes';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details-boxes',
   templateUrl: './details-boxes.component.html',
   styleUrl: './details-boxes.component.css'
 })
-export class DetailsBoxesComponent {
 
-  boxes: Array<Boxes>
-  constructor(private manageBoxesService: ManageBoxesService){
-  this.boxes = []
+  export class DetailsBoxesComponent {
 
-  this.manageBoxesService.getBoxes().subscribe((resultat)=>{
-  console.log(this.boxes)
-  this.boxes = resultat
-
-})
-}
-
-}
+    boxes: Array<Boxes>
+    laBox:Boxes|undefined
+    constructor(manageBoxesServices: ManageBoxesService, private route: ActivatedRoute) {
+      this.boxes = []
+      manageBoxesServices.getBoxes().subscribe((resultat) => {
+        this.boxes = resultat
+        const boxeId = Number(this.route.snapshot.paramMap.get('id'));
+       this.laBox= this.boxes.find((uneBox)=>uneBox.id==boxeId)
+        console.log(this.boxes)
+      })
+    }
+  }
